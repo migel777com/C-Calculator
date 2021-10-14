889ea6aad7afbb2b590df0f4c53dfb285eeda2d7
migel777com <62992979+migel777com@users.noreply.github.com>
2021-10-14 22:43:32 +0600

    Добавьте файлы проекта.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.num_7 = new System.Windows.Forms.Button();
+            this.num_8 = new System.Windows.Forms.Button();
+            this.num_9 = new System.Windows.Forms.Button();
+            this.num_4 = new System.Windows.Forms.Button();
+            this.num_5 = new System.Windows.Forms.Button();
+            this.num_6 = new System.Windows.Forms.Button();
+            this.num_1 = new System.Windows.Forms.Button();
+            this.num_2 = new System.Windows.Forms.Button();
+            this.num_3 = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.num_0 = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mult = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(283, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(214, 44);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // num_7
+            // 
+            this.num_7.Location = new System.Drawing.Point(283, 124);
+            this.num_7.Name = "num_7";
+            this.num_7.Size = new System.Drawing.Size(49, 46);
+            this.num_7.TabIndex = 1;
+            this.num_7.Text = "7";
+            this.num_7.UseVisualStyleBackColor = true;
+            this.num_7.Click += new System.EventHandler(this.num_7_Click);
+            // 
+            // num_8
+            // 
+            this.num_8.Location = new System.Drawing.Point(338, 124);
+            this.num_8.Name = "num_8";
+            this.num_8.Size = new System.Drawing.Size(49, 46);
+            this.num_8.TabIndex = 2;
+            this.num_8.Text = "8";
+            this.num_8.UseVisualStyleBackColor = true;
+            this.num_8.Click += new System.EventHandler(this.num_8_Click);
+            // 
+            // num_9
+            // 
+            this.num_9.Location = new System.Drawing.Point(393, 124);
+            this.num_9.Name = "num_9";
+            this.num_9.Size = new System.Drawing.Size(49, 46);
+            this.num_9.TabIndex = 3;
+            this.num_9.Text = "9";
+            this.num_9.UseVisualStyleBackColor = true;
+            this.num_9.Click += new System.EventHandler(this.num_9_Click);
+            // 
+            // num_4
+            // 
+            this.num_4.Location = new System.Drawing.Point(283, 176);
+            this.num_4.Name = "num_4";
+            this.num_4.Size = new System.Drawing.Size(49, 46);
+            this.num_4.TabIndex = 4;
+            this.num_4.Text = "4";
+            this.num_4.UseVisualStyleBackColor = true;
+            this.num_4.Click += new System.EventHandler(this.num_4_Click);
+            // 
+            // num_5
+            // 
+            this.num_5.Location = new System.Drawing.Point(338, 176);
+            this.num_5.Name = "num_5";
+            this.num_5.Size = new System.Drawing.Size(49, 46);
+            this.num_5.TabIndex = 5;
+            this.num_5.Text = "5";
+            this.num_5.UseVisualStyleBackColor = true;
+            this.num_5.Click += new System.EventHandler(this.num_5_Click);
+            // 
+            // num_6
+            // 
+            this.num_6.Location = new System.Drawing.Point(393, 176);
+            this.num_6.Name = "num_6";
+            this.num_6.Size = new System.Drawing.Size(49, 46);
+            this.num_6.TabIndex = 6;
+            this.num_6.Text = "6";
+            this.num_6.UseVisualStyleBackColor = true;
+            this.num_6.Click += new System.EventHandler(this.num_6_Click);
+            // 
+            // num_1
+            // 
+            this.num_1.Location = new System.Drawing.Point(283, 228);
+            this.num_1.Name = "num_1";
+            this.num_1.Size = new System.Drawing.Size(49, 46);
+            this.num_1.TabIndex = 7;
+            this.num_1.Text = "1";
+            this.num_1.UseVisualStyleBackColor = true;
+            this.num_1.Click += new System.EventHandler(this.num_1_Click);
+            // 
+            // num_2
+            // 
+            this.num_2.Location = new System.Drawing.Point(338, 228);
+            this.num_2.Name = "num_2";
+            this.num_2.Size = new System.Drawing.Size(49, 46);
+            this.num_2.TabIndex = 8;
+            this.num_2.Text = "2";
+            this.num_2.UseVisualStyleBackColor = true;
+            this.num_2.Click += new System.EventHandler(this.num_2_Click);
+            // 
+            // num_3
+            // 
+            this.num_3.Location = new System.Drawing.Point(393, 228);
+            this.num_3.Name = "num_3";
+            this.num_3.Size = new System.Drawing.Size(49, 46);
+            this.num_3.TabIndex = 9;
+            this.num_3.Text = "3";
+            this.num_3.UseVisualStyleBackColor = true;
+            this.num_3.Click += new System.EventHandler(this.num_3_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(283, 280);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(49, 46);
+            this.clear.TabIndex = 10;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // num_0
+            // 
+            this.num_0.Location = new System.Drawing.Point(338, 280);
+            this.num_0.Name = "num_0";
+            this.num_0.Size = new System.Drawing.Size(49, 46);
+            this.num_0.TabIndex = 11;
+            this.num_0.Text = "0";
+            this.num_0.UseVisualStyleBackColor = true;
+            this.num_0.Click += new System.EventHandler(this.num_0_Click);
+            // 
+            // equal
+            // 
+            this.equal.Location = new System.Drawing.Point(393, 280);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(49, 46);
+            this.equal.TabIndex = 12;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(448, 124);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(49, 46);
+            this.div.TabIndex = 13;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // mult
+            // 
+            this.mult.Location = new System.Drawing.Point(448, 176);
+            this.mult.Name = "mult";
+            this.mult.Size = new System.Drawing.Size(49, 46);
+            this.mult.TabIndex = 14;
+            this.mult.Text = "*";
+            this.mult.UseVisualStyleBackColor = true;
+            this.mult.Click += new System.EventHandler(this.mult_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(448, 228);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(49, 46);
+            this.sub.TabIndex = 15;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(448, 280);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(49, 46);
+            this.add.TabIndex = 16;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.mult);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.num_0);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.num_3);
+            this.Controls.Add(this.num_2);
+            this.Controls.Add(this.num_1);
+            this.Controls.Add(this.num_6);
+            this.Controls.Add(this.num_5);
+            this.Controls.Add(this.num_4);
+            this.Controls.Add(this.num_9);
+            this.Controls.Add(this.num_8);
+            this.Controls.Add(this.num_7);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button num_7;
+        private System.Windows.Forms.Button num_8;
+        private System.Windows.Forms.Button num_9;
+        private System.Windows.Forms.Button num_4;
+        private System.Windows.Forms.Button num_5;
+        private System.Windows.Forms.Button num_6;
+        private System.Windows.Forms.Button num_1;
+        private System.Windows.Forms.Button num_2;
+        private System.Windows.Forms.Button num_3;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button num_0;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mult;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button add;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public float first;
+        public float last;
+        public string operand;
+        public bool result = false;
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "MeNEvFoZ4DjwphzrtZDvyh3rPLD8wZvbgogB2Qrt",
+            BasePath = "https://calculator-32ec4-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void num_1_Click(object sender, EventArgs e)
+        {
+            if (result) 
+            {
+                label1.Text ="1";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "1";
+        }
+
+        private void num_2_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "2";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "2";
+        }
+
+        private void num_3_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "3";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "3";
+        }
+
+        private void num_4_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "4";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "4";
+        }
+
+        private void num_5_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "5";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "5";
+        }
+
+        private void num_6_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "6";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "6";
+        }
+
+        private void num_7_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "7";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "7";
+        }
+
+        private void num_8_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "8";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "8";
+        }
+
+        private void num_9_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "9";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "9";
+        }
+
+        private void num_0_Click(object sender, EventArgs e)
+        {
+            if (result)
+            {
+                label1.Text = "0";
+                result = false;
+                return;
+            }
+            label1.Text = label1.Text + "0";
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            if (!label1.Equals("")) 
+            {
+                first = float.Parse(label1.Text);
+                label1.Text = "";
+                operand = "add";
+            }
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            if (!label1.Equals(""))
+            {
+                first = float.Parse(label1.Text);
+                label1.Text = "";
+                operand = "sub";
+            }
+        }
+
+        private void mult_Click(object sender, EventArgs e)
+        {
+            if (!label1.Equals(""))
+            {
+                first = float.Parse(label1.Text);
+                label1.Text = "";
+                operand = "mult";
+            }
+        }
+
+        private void div_Click(object sender, EventArgs e)
+        {
+            if (!label1.Equals(""))
+            {
+                first = float.Parse(label1.Text);
+                label1.Text = "";
+                operand = "div";
+            }
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            if (!label1.Equals("") && first != 0) 
+            {
+                last = float.Parse(label1.Text);
+                float res = 0;
+
+                switch (operand) 
+                {
+                    case "add":
+                        res = first + last;
+                        label1.Text = res.ToString();
+                        break;
+                    case "sub":
+                        res = first - last;
+                        label1.Text = res.ToString();
+                        break;
+                    case "mult":
+                        res = first * last;
+                        label1.Text = res.ToString();
+                        break;
+                    case "div":
+                        res = first / last;
+                        label1.Text = res.ToString();
+                        break;
+                }
+
+                SetResponse set = client.Set(@"Calculation Results/" + res, res);
+
+                last = 0;
+                first = 0;
+                operand = "";
+                result = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if (client != null) 
+            {
+                MessageBox.Show("Connection is established");
+            }
+        }
+    }
+}
